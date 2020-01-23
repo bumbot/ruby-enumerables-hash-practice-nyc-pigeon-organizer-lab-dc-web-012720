3a4a299d6e7aea323a9de6bfbf51c332c10be28d
Paul Kim <pkim1295@gmail.com>
2020-01-23 22:07:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,7 @@ def nyc_pigeon_organizer(data)
   new_pigeon_data = get_attribute(new_pigeon_data, data, :color)
   new_pigeon_data = get_attribute(new_pigeon_data, data, :lives)
   
-  
+  pp new_pigeon_data
   new_pigeon_data
 end
 
